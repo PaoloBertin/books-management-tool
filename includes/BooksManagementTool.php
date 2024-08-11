617214438bffeb7daa79b07169deaa770c62a3e3
paolo.bertin <paolo.bertin.62@gmail.com>
2024-08-11 15:25:02 +0200

    modificato nome file class-books-management-tool-public

--- includes/BooksManagementTool.php
+++ includes/BooksManagementTool.php
@@ -120,7 +120,7 @@ class BooksManagementTool {
 		 * The class responsible for defining all actions that occur in the public-facing
 		 * side of the site.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-books-management-tool-public.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/BooksManagementToolPublic.php';
 
 		$this->loader = new BooksManagementToolLoader();
 
